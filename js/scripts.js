d9e8d0e6def702a823e0ce5543c09805e6f936dd
adem-max <53536875+adem-max@users.noreply.github.com>
2025-05-30 23:51:15 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,12 @@ const products = [
   { id: 1, name: "1957 Postage Stamp", price: 200, img: "images/stamp-1.jpg" },
   { id: 2, name: "1962 Independence Stamp", price: 450, img: "images/stamp-2.jpg" },
   { id: 3, name: "National Flower Stamp", price: 300, img: "images/stamp-3.jpg" },
-  { id: 4, name: "Historic Landmark Stamp", price: 350, img: "images/stamp-4.jpg" }
-  { id: 1, name: "1957 Postage Stamp", price: 200, img: "images/stamp-5.jpg" },
-  { id: 2, name: "1962 Independence Stamp", price: 450, img: "images/stamp-6.jpg" },
-  { id: 3, name: "National Flower Stamp", price: 300, img: "images/stamp-7.jpg" },
-  { id: 4, name: "Historic Landmark Stamp", price: 350, img: "images/stamp-8.jpg" }
+  { id: 4, name: "Historic Landmark Stamp", price: 350, img: "images/stamp-4.jpg" },
+  { id: 5, name: "1957 Postage Stamp Variant", price: 200, img: "images/stamp-5.jpg" },
+  { id: 6, name: "1962 Independence Stamp Variant", price: 450, img: "images/stamp-6.jpg" },
+  { id: 7, name: "National Flower Stamp Variant", price: 300, img: "images/stamp-7.jpg" },
+  { id: 8, name: "Historic Landmark Stamp Variant", price: 350, img: "images/stamp-8.jpg" }
 ];
-
 let cart = [];
 
 function renderProducts() {
